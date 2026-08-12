9755285cf2b9d32161d85f57ecb1daa447cfb2f9
Pinki Priya <pinkipriyam8@gmail.com>
2026-08-12 11:46:06 +0530

    fix: Research link opens in same tab

--- frontend/src/pages/Footer/Footer.jsx
+++ frontend/src/pages/Footer/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
               <Link to={FOUNDATION_BASE_URL}>
                 <span className="font">FOUNDATION</span>
               </Link>
-              <a href={`${RESEARCH_BASE_URL}/research/browseResearch`} target="_blank" rel="noopener noreferrer">
+              <a href={`${RESEARCH_BASE_URL}/research/browseResearch`}>
                 {" "}
                 <span>RESEARCH</span>
               </a>
@@ -134,7 +134,7 @@ const Footer = () => {
             <Link to="/news">BLOGS & NEWS</Link>
             <Link to={FOUNDATION_BASE_URL}><span className="font">FOUNDATION</span></Link> 
             <a href="https://careers.infinitohq.com" target="_blank" rel="noopener noreferrer">CAREER</a>
-             <a href={`${RESEARCH_BASE_URL}/research/browseResearch`} target="_blank" rel="noopener noreferrer"> <span>RESEARCH</span></a>
+             <a href={`${RESEARCH_BASE_URL}/research/browseResearch`}> <span>RESEARCH</span></a>
             <span>INTERNSHIP</span>
             <Link to="/support-us">SUPPORT US</Link>
           </div>
