fd4574f487cf6b62dfede0c65c6a5cabd1a0317f
Pinki Priya <pinkipriyam8@gmail.com>
2026-09-04 19:20:48 +0530

    fix: raise character z-index to z-40 so they appear in front of the login card

--- frontend/src/pages/Signup/SignupWrapper.jsx
+++ frontend/src/pages/Signup/SignupWrapper.jsx
@@ -112,13 +112,13 @@ const SignupWrapper = () => {
           <img
             src={Bullet}
             alt="Bullet"
-            className="absolute bottom-0 h-[80vh] z-20 object-contain pointer-events-none hidden lg:block"
+            className="absolute bottom-0 h-[80vh] z-40 object-contain pointer-events-none hidden lg:block"
             style={{ left: 'calc(50% - 540px)' }}
           />
           <img
             src={Riza}
             alt="Riza"
-            className="absolute bottom-0 h-[80vh] z-20 object-contain pointer-events-none hidden lg:block"
+            className="absolute bottom-0 h-[80vh] z-40 object-contain pointer-events-none hidden lg:block"
             style={{ right: 'calc(50% - 540px)' }}
           />
         </>
