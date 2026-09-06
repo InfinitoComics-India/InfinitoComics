061cd3682b54c06633d0999437ec459937ce46fa
Anushkaxx07 <anushkaupadhyay0705@gmail.com>
2026-09-07 01:42:00 +0530

    feat(news): display featured characters in place of spotlight, remove foundation section

--- frontend/src/pages/News_Blogs/News.jsx
+++ frontend/src/pages/News_Blogs/News.jsx
@@ -2,21 +2,17 @@ import React from 'react'
 import JoinUltimate from '../Home/JoinUltimate'
 import ArcheryNews from './Archery_News'
 import All_news from './All_news'
-import FoundationSection from './Foundation_Section'
-import ArcherySlider from './ArcherySlider'
-import Spotlight from '../../components/spotlight/Spotlight.jsx'
-import Carousel from '../News_Blogs/Carousel'
+import FeaturedCharactersCarousel from '../Characters/CharacterCarousel'
+import Carousel from './Carousel'
+
 const News = () => {
   return (
     <div>
-        <Carousel></Carousel>
-        <ArcheryNews />
-        <ArcherySlider/>
-        <Spotlight/>
-        <All_news/>
-        <FoundationSection/>
-        <ArcherySlider/>
-        <JoinUltimate/>
+      <Carousel />
+      <ArcheryNews />
+      <FeaturedCharactersCarousel />
+      <All_news />
+      <JoinUltimate />
     </div>
   )
 }
